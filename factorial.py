4e289f4e602541b62f0fbaa4889005e009d66a03
shashank123409 <kumarshashank527@gmail.com>
2025-05-14 00:14:56 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the number:"))
+fac=1
+for i in range(1,n+1):
+    fac*=i
+print(f"Factorial of {n} is:{fac}")
---
+++ square.py
@@ -0,0 +1,8 @@
+import math
+n=int(input("Enter a number:"))
+sq=math.sqrt(n)
+log=math.log(n)
+sin=math.sin(n)
+print("Square root:",sq)
+print("Logarithm:",log)
+print("Sine:",sin)
