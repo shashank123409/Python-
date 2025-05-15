edd5ade6bf93955fd1b56ec51f22e3526e2fece9
shashank123409 <kumarshashank527@gmail.com>
2025-05-15 22:44:18 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+l=[1,2,3,4,5,6,7,8,9,10]
+first_five=l[0:5]
+print("Original list:",l)
+print("Extracted first five elements:",first_five)
+rev=first_five[::-1]
+print("Reversed extracted elements:",rev)
